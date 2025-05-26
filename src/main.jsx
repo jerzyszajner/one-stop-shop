e2a085d905a42f7d90bbb3461813591b51b22b33
jerzy szajner <jerzy.szajner@gmail.com>
2025-05-26 20:22:43 +0200

    implement auth context

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <RouterProvider router={router} />
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
   // </StrictMode>,
 );
