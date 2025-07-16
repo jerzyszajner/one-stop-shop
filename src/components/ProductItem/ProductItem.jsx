ead6f7cfc25b336d267cf01943c0014726a8af11
jerzy szajner <jerzy.szajner@gmail.com>
2025-07-16 13:49:53 +0200

    improve application styling and layout

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -14,36 +14,38 @@ const ProductItem = ({ product }) => {
   return (
     <div className={styles.productCard}>
       {/* Product image */}
-      <div className={styles.productContainer}>
+      <div className={styles.imageContainer}>
         <img
           src={product.thumbnail}
           alt={product.title}
           className={styles.productImage}
         />
       </div>
+      <div className={styles.productInfoContainer}>
+        {/*----------------Product Title----------------*/}
+        <h2 className={styles.productTitle}>{product.title}</h2>
 
-      {/*----------------Product Title----------------*/}
-      <h2 className={styles.productTitle}>{product.title}</h2>
+        {/*----------------Product Brand----------------*/}
+        <p className={styles.productBrand}>
+          <b>Product Brand</b> {product.brand}
+        </p>
 
-      {/*----------------Product Brand----------------*/}
-      <p className={styles.productBrand}>
-        <b>Product Brand</b> {product.brand}
-      </p>
+        {/*----------------Rating----------------*/}
+        <p className={styles.productRating}>
+          <b>Rating:</b> {product.rating}
+        </p>
 
-      {/*----------------Rating----------------*/}
-      <p className={styles.productRating}>
-        <b>Rating:</b> {product.rating}
-      </p>
+        {/*----------------Price----------------*/}
+        <p className={styles.productPrice}>
+          <b>Price:</b> {product.price}
+        </p>
 
-      {/*----------------Price----------------*/}
-      <p className={styles.productPrice}>
-        <b>Price:</b> {product.price}
-      </p>
-
-      {/*----------------Discount----------------*/}
-      <p className={styles.discountPercentage}>
-        <b>Discount Percentage:</b> {product.discountPercentage}
-      </p>
+        {/*----------------Discount----------------*/}
+        <p className={styles.discountPercentage}>
+          <b>Discount Percentage:</b> {product.discountPercentage}
+        </p>
+      </div>
+      {/*----------------Buttons----------------*/}
       <div className={styles.buttonsContainer}>
         {/* Product actions */}
         <ButtonLink
